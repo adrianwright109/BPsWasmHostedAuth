8ef94007fb25151f34262f490c9c0c7ea2534add
Adrian Wright <adrian@compiledcoding.co.uk>
2021-04-14 13:06:13 +0100

    Add project files.

---
+++ BPsWasmHostedAuth/Server/Models/ApplicationUser.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BPsWasmHostedAuth.Server.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+    }
+}
